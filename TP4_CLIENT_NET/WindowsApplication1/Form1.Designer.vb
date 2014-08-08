fb419e2d8750fd8009b8d1f2db58d3cf512721b9
leseulsteve <leseulsteve@gmail.com>
2014-08-08 12:18:16 -0400

    Création du Client .NET
    Seulement la vue de fait

---
+++ TP4_CLIENT_NET/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.VScrollBar1 = New System.Windows.Forms.VScrollBar()
+        Me.TitreSectionChambre = New System.Windows.Forms.Label()
+        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.AddButton = New System.Windows.Forms.Label()
+        Me.cancelButton = New System.Windows.Forms.Label()
+        Me.VScrollBar2 = New System.Windows.Forms.VScrollBar()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer2.Panel1.SuspendLayout()
+        Me.SplitContainer2.Panel2.SuspendLayout()
+        Me.SplitContainer2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.ListView1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.VScrollBar1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TitreSectionChambre)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.SplitContainer2)
+        Me.SplitContainer1.Size = New System.Drawing.Size(878, 687)
+        Me.SplitContainer1.SplitterDistance = 423
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(16, 50)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(375, 628)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'VScrollBar1
+        '
+        Me.VScrollBar1.Location = New System.Drawing.Point(394, 50)
+        Me.VScrollBar1.Name = "VScrollBar1"
+        Me.VScrollBar1.Size = New System.Drawing.Size(18, 628)
+        Me.VScrollBar1.TabIndex = 1
+        '
+        'TitreSectionChambre
+        '
+        Me.TitreSectionChambre.AutoSize = True
+        Me.TitreSectionChambre.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TitreSectionChambre.Location = New System.Drawing.Point(13, 13)
+        Me.TitreSectionChambre.Name = "TitreSectionChambre"
+        Me.TitreSectionChambre.Size = New System.Drawing.Size(139, 31)
+        Me.TitreSectionChambre.TabIndex = 0
+        Me.TitreSectionChambre.Text = "Chambres"
+        '
+        'SplitContainer2
+        '
+        Me.SplitContainer2.Location = New System.Drawing.Point(3, 3)
+        Me.SplitContainer2.Name = "SplitContainer2"
+        Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer2.Panel1
+        '
+        Me.SplitContainer2.Panel1.Controls.Add(Me.Label3)
+        Me.SplitContainer2.Panel1.Controls.Add(Me.Label2)
+        Me.SplitContainer2.Panel1.Controls.Add(Me.Label1)
+        '
+        'SplitContainer2.Panel2
+        '
+        Me.SplitContainer2.Panel2.Controls.Add(Me.AddButton)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.cancelButton)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.VScrollBar2)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.TableLayoutPanel1)
+        Me.SplitContainer2.Size = New System.Drawing.Size(444, 681)
+        Me.SplitContainer2.SplitterDistance = 348
+        Me.SplitContainer2.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(18, 113)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(113, 37)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(211, 26)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Numéro de chambre"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(18, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Détails"
+        '
+        'AddButton
+        '
+        Me.AddButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.AddButton.AutoSize = True
+        Me.AddButton.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.AddButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.AddButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AddButton.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.AddButton.Location = New System.Drawing.Point(221, 277)
+        Me.AddButton.Name = "AddButton"
+        Me.AddButton.Padding = New System.Windows.Forms.Padding(4)
+        Me.AddButton.Size = New System.Drawing.Size(184, 34)
+        Me.AddButton.TabIndex = 3
+        Me.AddButton.Text = "Ajouter Réservation"
+        '
+        'cancelButton
+        '
+        Me.cancelButton.AutoSize = True
+        Me.cancelButton.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.cancelButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.cancelButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cancelButton.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.cancelButton.Location = New System.Drawing.Point(21, 277)
+        Me.cancelButton.Name = "cancelButton"
+        Me.cancelButton.Padding = New System.Windows.Forms.Padding(4)
+        Me.cancelButton.Size = New System.Drawing.Size(191, 34)
+        Me.cancelButton.TabIndex = 2
+        Me.cancelButton.Text = "Annuler Réservation"
+        '
+        'VScrollBar2
+        '
+        Me.VScrollBar2.Location = New System.Drawing.Point(417, 4)
+        Me.VScrollBar2.Name = "VScrollBar2"
+        Me.VScrollBar2.Size = New System.Drawing.Size(21, 257)
+        Me.VScrollBar2.TabIndex = 1
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(4, 4)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(406, 257)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(878, 687)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.PerformLayout()
+        Me.SplitContainer2.Panel2.ResumeLayout(False)
+        Me.SplitContainer2.Panel2.PerformLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents TitreSectionChambre As System.Windows.Forms.Label
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents VScrollBar1 As System.Windows.Forms.VScrollBar
+    Friend WithEvents SplitContainer2 As System.Windows.Forms.SplitContainer
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents VScrollBar2 As System.Windows.Forms.VScrollBar
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents AddButton As System.Windows.Forms.Label
+    Friend WithEvents cancelButton As System.Windows.Forms.Label
+
+End Class
---
+++ TP4_CLIENT_NET/WindowsApplication1/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    
+    Private Sub AddButton_Click(sender As Object, e As EventArgs) Handles AddButton.Click
+        Console.WriteLine("Hello world")
+    End Sub
+End Class
