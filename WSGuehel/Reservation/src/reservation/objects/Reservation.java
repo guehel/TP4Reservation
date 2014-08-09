cc374da7c6fdec3d2cabf5baead5fae9f8bc9401
guehel <guehel@yahoo.fr>
2014-08-09 08:59:21 -0400

    synchronization avec git

--- WSGuehel/Reservation/src/reservation/objects/Reservation.java
+++ WSGuehel/Reservation/src/reservation/objects/Reservation.java
@@ -63,11 +63,12 @@ public class Reservation implements Comparable<Reservation> {
 		public void setIdReservation(int idReservation) {
 			this.idReservation = idReservation;
 		}
-
+		//adds the specified element e to this set
+		//if the set contains no element e2 such that (e==null ? e2==null : e.equals(e2)).
 		@Override
 		public boolean equals(Object obj) {
 			Reservation resa = (Reservation) obj;
-			boolean retour = OutilsDates.CompareInterval(interval, resa.getInterval()) 
+			boolean retour = interval.abuts(resa.getInterval())
 					&& chambre.equals(resa.getChambre());
 			return retour;
 		}
@@ -88,9 +89,15 @@ public class Reservation implements Comparable<Reservation> {
 
 		@Override
 		public int compareTo(Reservation o) {
-			Long debut = interval.getStartMillis()/3600 + client.getIdClient();
-			Long fin = o.getInterval().getStartMillis()/3600 + o.getClient().getIdClient();
-			return (int) (fin - debut);
+			//verifier si les 2 intervals se touchent
+			boolean isDisjoint = !this.getInterval().abuts( o.getInterval());
+			int disjoint = isDisjoint? 1:0;
+			// Calcule le numchambre * debut 
+			Long debut = this.getInterval().getStartMillis()/3600 * this.getChambre().getNumeroChambre();
+			Long fin =  o.getInterval().getStartMillis()/3600 * o.getChambre().getNumeroChambre();
+			// pour une meme chambre retourne 0, si les intervals ne sont pas disjoint
+			//retour = (numchambre * debut - numchambre * debut) * disjoint
+			return (int) (fin - debut)*disjoint;
 		}
 	    
 		
